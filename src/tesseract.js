02e9f82687fc02d777fbebe8527c40516686531f
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-03-23 15:38:56 -0400

    add whitelist for hiragana characters only

--- src/tesseract.js
+++ src/tesseract.js
@@ -1,6 +1,12 @@
 document.getElementById('check').onclick = function (e) {
   var context = document.getElementById('canvas').getContext('2d');
-  Tesseract.recognize(context, 'jpn')
+
+  var tessOptions = {
+    lang: 'jpn',
+    tessedit_char_whitelist: [ 'ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん', 'ゔ', 'ゕ', 'ゖ' ]
+  }
+
+  Tesseract.recognize(context, tessOptions)
     .catch(function (error) {
       console.log(error);
     })
